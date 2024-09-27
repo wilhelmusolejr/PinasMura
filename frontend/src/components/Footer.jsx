d024fe5342b9726907f199bcbea2b92bdbb13b7d
Wilhelmus Ole Jr <wilhelmus.olejr@gmail.com>
2024-09-27 10:00:39 +0800

    added links in footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,12 +5,13 @@ const github = import.meta.env.VITE_SOCIAL_GITHUB;
 export default function Footer() {
   return (
     <footer>
-      <div className="container mx-auto mt-60 flex items-center justify-between gap-3 px-5 pb-10">
+      {/* <div className="container flex items-center justify-between gap-3 px-5 pb-10 mx-auto mt-60"> */}
+      <div className="container mx-auto mt-60 flex flex-col-reverse items-center justify-center gap-6 px-5 pb-10 md:flex-row md:justify-between">
         <p className="text-center text-xs">
           {`© 2021 PinasMura. All Rights Reserved. Terms of Service Privacy
           Policy`}
         </p>
-        <div className="flex gap-4">
+        <div className="flex flex-wrap justify-start gap-4">
           <a target="_blank" href={portfolio}>
             Portfolio
           </a>
